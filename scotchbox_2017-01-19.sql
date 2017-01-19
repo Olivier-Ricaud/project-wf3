8da84bc7f9bdaec927dcdca190357c70820ac79a
thomaspiu <thomas.piussan@gmail.com>
2017-01-19 10:09:19 +0100

    Update sql file : add photo in the salles table

--- scotchbox_2017-01-19.sql
+++ scotchbox_2017-01-19.sql
@@ -7,7 +7,7 @@
 #
 # Host: 127.0.0.1 (MySQL 5.5.46-0ubuntu0.14.04.2)
 # Database: scotchbox
-# Generation Time: 2017-01-19 08:50:38 +0000
+# Generation Time: 2017-01-19 09:08:23 +0000
 # ************************************************************
 
 
@@ -18,88 +18,6 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
-
-# Dump of table chats
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `chats`;
-
-CREATE TABLE `chats` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `messages` text,
-  `pseudo` varchar(100) DEFAULT NULL,
-  `event_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-
-# Dump of table contacts
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `contacts`;
-
-CREATE TABLE `contacts` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `email` varchar(255) DEFAULT NULL,
-  `sujet` varchar(255) DEFAULT NULL,
-  `contenu` text,
-  `date` datetime DEFAULT NULL,
-  `user_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-
-# Dump of table events
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `events`;
-
-CREATE TABLE `events` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `titre` varchar(100) DEFAULT NULL,
-  `date_heure` datetime DEFAULT NULL,
-  `sexe` enum('Homme','Femme') DEFAULT NULL,
-  `niveau` enum('Débutant','Intermédiaire','Confirmé') DEFAULT NULL,
-  `salle_id` int(11) DEFAULT NULL,
-  `host_id` int(11) DEFAULT NULL,
-  `chat_id` int(11) DEFAULT NULL,
-  `duree` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-
-# Dump of table joueurs
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `joueurs`;
-
-CREATE TABLE `joueurs` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) DEFAULT NULL,
-  `event_id` int(11) DEFAULT NULL,
-  `equipe_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-
-# Dump of table resultats
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `resultats`;
-
-CREATE TABLE `resultats` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `event_id` int(11) DEFAULT NULL,
-  `score_equipe_1` int(11) DEFAULT NULL,
-  `score_equipe_2` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 
 
 # Dump of table salles
@@ -115,26 +33,7 @@ CREATE TABLE `salles` (
   `adresse` varchar(255) DEFAULT NULL,
   `tarif` tinyint(3) DEFAULT NULL,
   `site_web` varchar(300) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-
-# Dump of table users
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `users`;
-
-CREATE TABLE `users` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `nom` varchar(50) DEFAULT NULL,
-  `prenom` varchar(50) DEFAULT NULL,
-  `pseudo` varchar(50) DEFAULT NULL,
-  `sexe` enum('Homme','Femme') DEFAULT NULL,
-  `departement` varchar(100) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `niveau` enum('Débutant','Intermédiaire','Confirmé') DEFAULT NULL,
-  `photo` varchar(255) DEFAULT NULL,
+  `photo` varchar(300) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
