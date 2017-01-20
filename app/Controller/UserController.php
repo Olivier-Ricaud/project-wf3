309ebdfbe321fdf9a62856d8af8e7bb722e854a0
Etudiant <Etudiant>
2017-01-20 11:14:00 +0100

    enregistrement ok(premier commit)

--- app/Controller/UserController.php
+++ app/Controller/UserController.php
@@ -29,45 +29,41 @@ class UserController extends Controller
 
 		if ( isset($_POST['sign-up']) ) {
 
-			$validator = new GUMP();
+			$gump = new GUMP();
 			$manager = new UserManager();
 
 			// Validation et Filtrage
 
-			$_POST['form_register_util'] = $validator->sanitize($_POST['form_register_util']); // You don't have to sanitize, but it's safest to do so.
-			$validator->validation_rules(array(
-				'nom'    => 'required|alpha_numeric|valid_name|min_len,2 |max_len,50',
-				'prenom'    => 'required|alpha_numeric|valid_name|min_len,2 |max_len,50',
-				'email'    => 'required|valid_email',
-				'sexe'    => 'required|exact_len,5|contains_list,Homme;Femme',
-				'lieu'    => ' required|alpha_dash|min_len,5|max_len,50',
-				'password'    => 'required|max_len,50|min_len,6',
-				'confirm_password'    => 'required|equalfield,password'
+			$_POST['form_register_util'] = $gump->sanitize($_POST['form_register_util']); // You don't have to sanitize, but it's safest to do so.
+			$gump->validation_rules(array(
+				'nom'                 => 'required|alpha_numeric|valid_name|max_len,50',
+				'prenom'              => 'required|alpha_numeric|valid_name|max_len,50',
+				'email'               => 'valid_email',
+				'sexe'                => 'required|exact_len,5|contains_list,Homme;Femme',
+				'lieu'                => 'alpha_dash|min_len,5|max_len,50',
+				'password'            => 'max_len,50|min_len,6',
+				'confirm_password'    => 'equalsfield,password'
 			));
 
-			$validator->filter_rules(array(
-				'nom' => 'sanitize_string|whole_number|trim',
-				'prenom'    => 'sanitize_string|whole_number|trim',
-				'email'    => 'trim|sanitize_email',
-				'sexe'    => ' trim|santize_string',
-				'lieu'    => 'sanitize_string|whole_number|trim',
-				'password'    => 'trim|base64_encode',
-				'confirm_password'    => 'trim|base64_encode'
+			$gump->filter_rules(array(
+				'nom'                 => 'sanitize_string|whole_number',
+				'prenom'              => 'sanitize_string|whole_number',
+				'email'               => 'sanitize_email',
+				// 'sexe'                => 'santize_string',
+				// 'lieu'                => 'sanitize_string|whole_number',
+				'password'            => 'base64_encode',
+				'confirm_password'    => 'base64_encode'
 			));
 
-			$_POST = $validator->filter($_POST);
+			$validated_data = $gump->run($_POST['form_register_util']);
 
-			// You can run filter() or validate() first
+			// Si validation non Ok
+			if($validated_data === false) {
+			    echo $gump->get_readable_errors(true);
+			} else {
+			    print_r($validated_data); // validation successful
 
-			$validated = $validator->validate(
-				$_POST, $rules
-			);
-
-			// Si validation Ok
-
-			if($validated === TRUE)
-			{
-				echo "Successful Validation\n\n";
+			    echo "Successful Validation\n\n";
 				
 				print_r($_POST); // You can now use POST data safely
 				
@@ -80,16 +76,7 @@ class UserController extends Controller
 				$utilisateur->insert($_POST['form_register_util']);
 				$this->redirectToRoute('home');
 			}
-			else
-			{
-				print_r($_POST); 
-					
-				print_r($validated); // Shows all the rules that failed along with the data
-			}
-
-			// Fin Validation et Filtrage
-
-			
+			// Fin Validation et Filtrage	
 
 		} else {
 			$this->show('user/register');
