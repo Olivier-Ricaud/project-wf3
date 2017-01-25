179902a13c4acc8d47285bd0e44eae1bef950254
Etudiant <Etudiant>
2017-01-25 15:12:37 +0100

    test

--- app/Manager/EventManager.php
+++ app/Manager/EventManager.php
@@ -18,10 +18,10 @@ class EventManager extends \W\Manager\Manager {
 	function findEvents($departement, $d_Date, $f_Date, $niveau, $sexe, $duree) {
 
 		
-		//SELECT * FROM events e INNER JOIN salles s ON e.salle_id = s.id WHERE s.departement='Val-d-Oise' AND (date BETWEEN '2017-01-01' AND '2017-01-31') AND e.niveau='Intermédiaire' AND e.sexe='Homme' AND e.duree='02:00'
+		//SELECT *, e.id FROM events e INNER JOIN salles s ON e.salle_id = s.id WHERE s.departement='Val-d-Oise' AND (date BETWEEN '2017-01-01' AND '2017-01-31') AND e.niveau='Intermédiaire' AND e.sexe='Homme' AND e.duree='02:00'
 
 
-		$sql = "SELECT * FROM events e INNER JOIN salles s ON e.salle_id = s.id WHERE ";
+		$sql = "SELECT *, e.id FROM events e INNER JOIN salles s ON e.salle_id = s.id WHERE ";
 
 		if (!empty($departement) && preg_match('/^[a-zA-Z0-9-]+$/', $departement)){
 
